61f9627bd714b04fdbcc7ebb416bb0f4a52466ed
SURBHI SHEKHAR <79759981+ssurbhi09@users.noreply.github.com>
2021-10-04 23:18:51 +0530

    Add files via upload

---
+++ assending.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int arr[100];
+    int size, i, j, temp;
+ 
+    // Reading the size of the array
+    cout<<"Enter size of array: ";
+    cin>>size;
+ 
+    //Reading elements of array
+    cout<<"Enter elements in array: ";
+    for(i=0; i<size; i++)
+    {
+        cin>>arr[i];
+    }
+    //Sorting an array in ascending order
+    for(i=0; i<size; i++)
+    {
+        for(j=i+1; j<size; j++)
+        {
+            //If there is a smaller element found on right of the array then swap it.
+            if(arr[j] < arr[i])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    //Printing the sorted array in ascending order
+    cout<<"Elements of array in sorted ascending order:"<<endl;
+    for(i=0; i<size; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+ 
+    return 0;
+}
+
